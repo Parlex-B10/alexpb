4645dde73810774b0112a6b891ccd4e94245c2d4
Parlex-B10 <83713872+Parlex-B10@users.noreply.github.com>
2024-05-15 15:42:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,70 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-import altair as alt
+import plotly.graph_objects as go
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from  PIL import Image
 
-# Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+st.set_page_config(page_title="Alejandro Paredes", page_icon=":bar_chart:", layout="wide")
+hide_st_style = """
+<style>
+#MainMenu {
+  visibility: hidden;
+  }
+footer {
+  visibility: hidden;
+  }
+header {
+  visibility: hidden;
+}
+</style>
+"""
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
-  
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
-# Load data
-df = pd.read_csv('data/movies_genres_summary.csv')
-df.year = df.year.astype('int')
+st.header('**Alejandro Paredes Balgañón**')
+st.header('**Uso de Plataformas de Streaming**')
+selected = option_menu(None, ["Dashboard","Reporte", "Mapa", "Sobre Mi", "Referencias"],
+    icons=['bar-chart','clipboard', 'map', "house", 'gear'],
+    menu_icon="cast", default_index=0, orientation="horizontal")
 
-# Input widgets
-## Genres selection
-genres_list = df.genre.unique()
-genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
-
-## Year selection
-year_list = df.year.unique()
-year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
-
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
+# Referencias
+if selected == "Referencias":
+  url_Streaming = "https://www.kaggle.com/datasets/rajatkumar30/streaming-application-viewership"
+  url_Countinent = "https://www.kaggle.com/datasets/andradaolteanu/country-mapping-iso-continent-region"
+  url_CodesCountry = "https://www.kaggle.com/datasets/andradaolteanu/iso-country-codes-global"
+  st.write(f'''Para este trabajo se utilizaron algunas bases de datos de kagle:
+  - [Conjunto de datos de Streaming]( {url_Streaming} )
+  - [Conjunto de datos de los Continetes]( {url_Countinent} )
+  - [Conjunto de datos de lo codigos de los países]( {url_CodesCountry} )
+  ''')
+  st.markdown('________________________')
 
 
-# Display DataFrame
+# Sobre Mi
+if selected == 'Sobre Mi':
 
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-                            column_config={"year": st.column_config.TextColumn("Year")},
-                            num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
+  st.markdown(""" <style> .font {
+  font-size:35px ; font-family: 'Cooper Black'; color: #8eff8e;}
+  </style> """,unsafe_allow_html=True)
+  st.markdown('<p class="font">Sobre mi 👨‍🎓💻</p>', unsafe_allow_html=True)
 
-# Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-            x=alt.X('year:N', title='Year'),
-            y=alt.Y('gross:Q', title='Gross earnings ($)'),
-            color='genre:N'
-            ).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
+  st.write(f'''Bienvenidos a mi página web, yo soy Alejandro Paredes Balgañon.''')
+  st.write(f'''Estudiante de séptimo semestre de ingeniería en transformación digital de negocios, interesado en la parte de análisis y visualización de los datos.
+            Me considero una persona organizada, responsable, capaz de resolver cualquier problema, y
+            busco adquirir nuevas habilidades que me permitan seguir desarrollándome como persona y como profesional.
+            ''')
+
+# Reporte
+if selected == 'Reporte':
+  st.header("Reporte :clipboard:")
+  st.markdown('________________________')
+
+# Mapa
+if selected == 'Mapa':
+  st.header("Mapa :world_map:")
+  st.markdown('________________________')
+
+# Dashboard
+if selected == 'Dashboard':
+  st.header("Dashboard :bar_chart:")
